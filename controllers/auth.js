730c8516c0a5e16a14a2805e9c3c6c2742153dcf
Juan Camba <juan_camba@yahoo.com.ar>
2024-12-28 11:28:01 +0100

    end 382

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,35 @@
 const {response} = require('express');
 const { validationResult } = require('express-validator')
 
-const crearUsuario = (req, res=  response)=>{
+const Usuario = require('../models/Usuario')
+
+const crearUsuario = async (req, res=  response)=>{
     
     const {name, email, password} = req.body;
     
-    
-    // errorees en middleware
-    
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
+ 
 
+    try {
+        const usuario = new Usuario(req.body);
 
-    });
+        await usuario.save();
+        // errorees en middleware
+        
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+            name,
+            email,
+            password
+    
+    
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: "Error al guardar en bd"
+        })
+    }
 
 }
 
