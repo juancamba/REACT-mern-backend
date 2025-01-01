5bcbbb272787a2bae3672dad2d53f34d831a5673
Juan Camba <juan_camba@yahoo.com.ar>
2025-01-01 19:49:02 +0100

    fix bd

--- database/config.js
+++ database/config.js
@@ -7,9 +7,9 @@ const mongoose = require('mongoose');
 const dbConnection = async() =>{
     try {
         mongoose.connect(process.env.DB_CNN),{
-            useNewUrlParser: true,
-            useUnifiedtopoly: true,
-            useCreateIndex: true
+           // useNewUrlParser: true,
+            //useUnifiedtopoly: true,
+            //useCreateIndex: true
         };
 
         console.log("bd online");
