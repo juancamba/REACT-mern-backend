c788913d74bd3db3fbbc8bb66fb7d07ac7f2b91b
Juan Camba <juan_camba@yahoo.com.ar>
2025-01-01 19:11:01 +0100

    end 399

--- controllers/events.js
+++ controllers/events.js
@@ -1,22 +1,42 @@
 const {response} = require('express');
-
+const Evento = require('../models/Evento')
 
 
 const getEventos = async (req, res=  response)=>{
 
-
+    // trae los eventos, ademas añede el objeto usuario los campos name y email
+    const eventos = await Evento.find().populate('user','name email');
 
 
     res.json({
         ok: true,
-        msg: 'getEventos',
+        msg: eventos,
        
     })
 };
 
 const crearEvento = async (req, res=  response)=>{
 
-    
+    const evento = new Evento( req.body);
+    try {
+        
+        evento.user = req.uid;
+        const eventoGuardado = await evento.save();
+        return res.json({
+            ok: true,
+            evento: eventoGuardado,
+           
+        })
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json(
+            {
+                ok: false,
+                msg: "Error al guardar"
+            }
+        )
+    }
 
     res.json({
         ok: true,
@@ -28,20 +48,96 @@ const crearEvento = async (req, res=  response)=>{
 
 const actualizarEvento = async (req, res=  response)=>{
 
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento',
-       
-    })
+    const eventoId = req.params.id;
+    const uid = req.uid;
+    try {
+        
+        const evento = await Evento.findById(eventoId);
+        if(!evento){
+            return res.status(404).json(
+            {
+                ok: false,
+                msg: "No existe ese evento"
+            });
+        }
+
+        if(evento.user.toString() !== uid){
+            return res.status(401).json(
+                {
+                    ok: false,
+                    msg: "No puede editar ese evento"
+                });
+        }
+        
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {new: true});
+
+        return res.json({
+            ok: true,
+            evento: eventoActualizado,
+           
+        })
+
+    } catch (error) {
+        console.error(error)
+        return  res.status(500).json(
+            {
+                ok: false,
+                msg: "Error al guardar"
+            }
+        )
+    }
+    
+  
 };
 
 const eliminarEvento = async (req, res=  response)=>{
+    const eventoId = req.params.id;
+    const uid = req.uid;
+    try {
+        
+        const evento = await Evento.findById(eventoId);
+        if(!evento){
+            return res.status(404).json(
+            {
+                ok: false,
+                msg: "No existe ese evento"
+            });
+        }
 
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento',
-       
-    })
+        if(evento.user.toString() !== uid){
+            return res.status(401).json(
+                {
+                    ok: false,
+                    msg: "No puede eliminar ese evento"
+                });
+        }
+        
+
+     
+
+        await Evento.findByIdAndDelete(eventoId);
+
+        return res.json({
+            ok: true
+        
+           
+        })
+
+    } catch (error) {
+        console.error(error)
+        return  res.status(500).json(
+            {
+                ok: false,
+                msg: "Error al guardar"
+            }
+        )
+    }
 };
 module.exports = {
     getEventos,
--- models/Evento.js
+++ models/Evento.js
@@ -11,16 +11,29 @@ const EventoSchema = Schema({
     },
     start: {
         type: Date,
-        require: true
+        required: true
     },
     end: {
         type: Date,
-        require: true
+        required: true
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'Usuario'
+        ref: 'Usuario',
+        required: true
     }
 })
+
+// sobreescibo el modelo para especificar lo que quiero retornar 
+
+EventoSchema.method('toJSON', function(){
+    
+    // quitamos __v, _id, la idea es solo dejar id en vez de ese guion bajo
+    const {__v, _id, ...object} = this.toObject();
+    // creamos el objeto id
+    object.id = _id;
+    return object;
+})
+
 
 module.exports = model('Evento', EventoSchema);
